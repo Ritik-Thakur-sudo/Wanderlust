4f88f214b851cbab5f2ec9d7d3cd8acd77c3a627
Ritik Thakur <rk7495933@gmail.com>
2025-08-04 00:41:47 +0530

    create a model called listing & add a route testListing

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const Listing = require("./models/listing.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
-  main()
+main()
   .then(() => {
     console.log("connect to DB");
   })
@@ -12,13 +13,25 @@ const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
     console.log(err);
   });
 
-
 async function main() {
   await mongoose.connect(MONGO_URL);
 }
 
 app.get("/", (req, res) => {
   res.send("i am root");
+});
+
+app.get("/testLising", async (req, res) => {
+  let sampleListing = new Listing({
+    title: "My Home",
+    description: "Good looking house",
+    price: 1200,
+    location: "Patna, Bihar",
+    country: "India",
+  });
+  await sampleListing.save();
+  console.log("sample was saved");
+  res.send("successfull testing");
 });
 
 app.listen(8080, () => {
---
+++ backend/models/listing.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    default: "https://images.unsplash.com/photo-1600585154340-be6161a56a0c",
+    set: (v) =>
+      v === ""
+        ? "https://images.unsplash.com/photo-1600585154340-be6161a56a0c"
+        : v,
+  },
+  price: Number,
+  location: String,
+  country: String,
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
