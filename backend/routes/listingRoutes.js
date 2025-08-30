ae0bb0dcc5aa0af3472ccc1c7574f517459853e6
Ritik Thakur <rk7495933@gmail.com>
2025-08-31 03:25:54 +0530

    implement image uploading and editing features

--- backend/routes/listingRoutes.js
+++ backend/routes/listingRoutes.js
@@ -8,16 +8,30 @@ const { isLoggedIn } = require("../middlewares/auth");
 const { validateListingId } = require("../middlewares/validateId");
 const { isOwner } = require("../middlewares/ownership");
 
+const multer = require("multer");
+const { storage } = require("../cloudConfig");
+const upload = multer({ storage });
+
 // Listing Routes
 router
   .route("/")
   .get(listingController.index) // Index Route
-  .post(isLoggedIn, listingController.create); // Create Route
+  .post(                        
+    isLoggedIn,
+    upload.single("image"),
+    listingController.create // Create Route
+  );
 
 router
   .route("/:id")
   .get(validateListingId, listingController.show) // Show Route
-  .put(isLoggedIn, validateListingId, isOwner, listingController.update) // Update Route
+  .put(
+    isLoggedIn,
+    validateListingId,
+    isOwner,
+    upload.single("image"), 
+    listingController.update // Update Route
+  )
   .delete(isLoggedIn, validateListingId, isOwner, listingController.delete); // Delete Route
 
 // Review Routes
@@ -28,6 +42,7 @@ router.post(
   reviewController.createReview
 );
 
+// Review Delete
 router.delete(
   "/:id/reviews/:reviewId",
   isLoggedIn,
