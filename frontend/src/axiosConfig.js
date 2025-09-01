fa4205c41e1fc6fc603b03e666c54deb6033048f
Ritik Thakur <rk7495933@gmail.com>
2025-09-02 01:25:04 +0530

    update: debugging for the deployment

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const baseURL = import.meta.env.VITE_BACKEND_URL || "http://localhost:8080";
 
-console.log("📡 Axios baseURL set to:", baseURL);
+console.log("Axios baseURL set to:", baseURL);
 
 const api = axios.create({
   baseURL,
